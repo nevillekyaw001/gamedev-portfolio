60e834b0354051a1b0852d7928361e6929181074
Aung Paing <aungpaing95@gmail.com>
2024-06-22 18:59:04 +0630

    update contact

---
+++ contact.php
@@ -0,0 +1,16 @@
+<form action="https://formsubmit.co/el/murito" class="form" method="post" data-form>
+
+<div class="input-wrapper">
+  <input type="text" name="fullname" class="form-input" placeholder="Full name" required data-form-input>
+
+  <input type="email" name="email" class="form-input" placeholder="Email address" required data-form-input>
+</div>
+
+<textarea name="message" class="form-input" placeholder="Your Message" required data-form-input></textarea>
+
+<button class="form-btn" type="submit">
+  <ion-icon name="paper-plane"></ion-icon>
+  <span>Send Message</span>
+</button>
+
+</form>
